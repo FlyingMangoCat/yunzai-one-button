1c00c508f8ea46489ef2a29fb3f6cc86f017c38b
会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>
2023-06-27 12:18:10 +0000

    update YZv3.sh.
    Signed-off-by: 会飞的芒果猫 <11234429+huifeidemangguomao@user.noreply.gitee.com>

--- YZv3.sh
+++ YZv3.sh
@@ -54,7 +54,7 @@ else
 fi
 }
 
-function rely on {
+function relyon {
 echo '正在安装依赖……'
 pnpm install -P
 }
@@ -85,7 +85,7 @@ read -p "请输入您要执行的操作数字：" choice
 case $choice in
   1) MangoCat-Yunzai ;;
   2) Miao-Yunzai ;;
-  3) rely on ;;
+  3) relyon ;;
   4) start ;;
   *) echo "~" ;;
 esac
